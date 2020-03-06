dbba0b7f421e8a050da8ab565e2c31867119b63a
Vovan932 <39453689+Vovan932@users.noreply.github.com>
2020-03-06 14:15:56 +0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,20 @@
-function setup()
-{
-	createCanvas(200, 200);
+function setup() {
+  createCanvas(displayWidth/2, displayHeight/2, WEBGL);
+  print(displayWidth/2);
+  print(displayHeight/2);
+  
 }
 
-
-
-function draw()
-{
-	
-	background(128)
-	ellipse(random(0,200), random (0,200), 80, 80);
-	noUpdate()
-
+function draw() {
+  background(128);  
+  translate(-50,0,200)
+  rotateX(frameCount * 0.05);  
+  strokeWeight(1);
+  if (frameCount % 30 == 0){
+    fill(random(0,255),random(0,255),random(0,255),random(0,255));
+  }  
+  box(40, 40, 40);  
+  translate(100,0,0)  
+  box(40, 40, 40);
+  
 }
