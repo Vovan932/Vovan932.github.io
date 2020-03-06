a31962096c47f1c2c54001e52bd7cf5ec8d2ba7e
Vovan932 <39453689+Vovan932@users.noreply.github.com>
2020-03-06 22:20:03 +0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,26 @@
 function setup() {
-  createCanvas(400, 400, WEBGL);  
+	//createCanvas(400, 400);
+	capture = createCapture(VIDEO);
+	capture.size(400, 400);
+
+
+	createCanvas(400, 400, WEBGL);  
+	console.log('ml5 version:', ml5.version);
+  
 }
 
 function draw() {
-  background(128);  
-  translate(-50,0,200)
-  rotateX(frameCount * 0.05);  
-  strokeWeight(1);
-  if (frameCount % 30 == 0){
-    fill(random(0,255),random(0,255),random(0,255),random(0,255));
-  }  
-  box(40, 40, 40);  
-  translate(100,0,0)  
-  box(40, 40, 40);
+	background(128);  
+	translate(-50,0,200);
+	rotateX(frameCount * 0.05);  
+	strokeWeight(1);
+	if (frameCount % 30 == 0){
+	fill(random(0,255),random(0,255),random(0,255),random(0,255));
+	}  
+	box(40, 40, 40);  
+	translate(100,0,0);  
+	box(40, 40, 40);
   
 }
+
+
